42087afb40e01046bf4707650e510d318464a387
Azucena Zhou <abbey-1825@hotmail.com>
2019-09-06 16:55:07 -0300

    ETHBA-42 DONE change icons

--- frontend/src/components/home/social/Social.js
+++ frontend/src/components/home/social/Social.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import { Grid } from '@material-ui/core'
 
+<<<<<<< HEAD
 import Meetup from '../../../assets/icons/social/meetup.svg'
 import Youtube from '../../../assets/icons/social/youtube.svg'
 import Telegram from '../../../assets/icons/social/telegram.svg'
+=======
+import Meetup from '../../../assets/social/meetup.svg'
+import Youtube from '../../../assets/social/youtube.svg'
+import Telegram from '../../../assets/social/telegram.svg'
+>>>>>>> ETHBA-42 DONE change icons
 import './social.scss'
 
 const iconSocial = [
@@ -45,11 +51,9 @@ export default class Social extends Component {
         <div className='container-social' container>
           {iconSocial.map(social => {
             return (
-              // <Grid key={social.icon} item xs={3} sm={2} md={2} lg={1} className='box-social'>
               <div className={`${social.class} box-social`}>
                 <img src={social.icon} alt={social.name} />
               </div>
-              // </Grid>
             )
           })}
         </div>
