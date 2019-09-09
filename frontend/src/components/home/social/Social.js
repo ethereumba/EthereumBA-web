4e36f1a30a3eaf33e67d717f88c9ed29e07e5086
Azucena Zhou <abbey-1825@hotmail.com>
2019-09-09 14:49:37 -0300

    ETHBA-44 WIP merge style change

--- frontend/src/components/home/social/Social.js
+++ frontend/src/components/home/social/Social.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
 import { Grid } from '@material-ui/core'
 
-<<<<<<< HEAD
-import Meetup from '../../../assets/icons/social/meetup.svg'
-import Youtube from '../../../assets/icons/social/youtube.svg'
-import Telegram from '../../../assets/icons/social/telegram.svg'
-=======
 import Meetup from '../../../assets/social/meetup.svg'
 import Youtube from '../../../assets/social/youtube.svg'
 import Telegram from '../../../assets/social/telegram.svg'
->>>>>>> ETHBA-42 DONE change icons
 import './social.scss'
 
 const iconSocial = [
@@ -36,22 +30,14 @@ export default class Social extends Component {
 
     return (
       <div className='social'>
-        {/* <Grid className='container-social' container>
-          {iconSocial.map(social => {
-            return (
-              <Grid key={social.icon} item xs={3} sm={2} md={2} lg={1} className='box-social'>
-                <div className={social.class}>
-                  <img src={social.icon} alt={social.name} />
-                </div>
-              </Grid> */}
-        {/* <div className={'sub-title'}>
+        <div className={'sub-title'}>
           <p>get in touch</p>
-        </div> */}
+        </div>
 
         <div className='container-social' container>
           {iconSocial.map(social => {
             return (
-              <div className={`${social.class} box-social`}>
+              <div key={social.icon} className={`${social.class} box-social`}>
                 <img src={social.icon} alt={social.name} />
               </div>
             )
