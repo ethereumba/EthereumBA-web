9d659416c0ed1f2827f597d139e8ff8822f19424
niconiahi <nicolas.accetta@gmail.com>
2019-09-25 17:27:20 -0300

    ETHB-53 WIP add header component

---
+++ frontend/src/components/eventDetail/EventDetailHeader/EventDetailHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventDetailHeader = () => (
+    <Grid item xs>
+        <span>EventDetailHeader</span>
+    </Grid>
+)
+
+export default EventDetailHeader
--- frontend/src/pages/eventDetail/EventDetail.js
+++ frontend/src/pages/eventDetail/EventDetail.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
+// material ui
+import Grid from '@material-ui/core/Grid'
+
 // proptypes
 
 // components
+import EventDetailHeader from '../../components/eventDetail/EventDetailHeader'
 
 // styles
 import './eventDetail.scss'
 
 const EventDetail = () => (
-      <div>
-        <span>EventDetail</span>
+      <div className='container'>
+          <Grid container>
+              <EventDetailHeader /> 
+          </Grid> 
       </div>
 
 ) 
