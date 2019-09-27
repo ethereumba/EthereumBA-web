82667fe6db74098d7689024554992efbb7c8d15e
niconiahi <nicolas.accetta@gmail.com>
2019-09-27 18:50:23 -0300

    ETHBA-56 WIP add info displayers and create grid

--- frontend/src/components/eventDetail/EventDetailTalkCard/DownloadMaterial/DownloadMaterial.js
+++ frontend/src/components/eventDetail/EventDetailTalkCard/DownloadMaterial/DownloadMaterial.js
@@ -9,15 +9,13 @@ import DownloadIcon from '../../../../assets/eventDetail/download-orange.svg';
 // lib
 import API_URL from '../../../../lib/api';
 
-const DownloadMaterial = ({ material }) => {
-  return (
-    <Container>
-      <IconWrapper href={`${API_URL}${material.file}`} target="_blank">
-        <Icon src={DownloadIcon} />
-      </IconWrapper>
-      <Text>{material.name}</Text>
-    </Container>
-  );
-};
+const DownloadMaterial = ({ material }) => (
+  <Container>
+    <IconWrapper href={`${API_URL}${material.file}`} target="_blank">
+      <Icon src={DownloadIcon} />
+    </IconWrapper>
+    <Text>{material.name}</Text>
+  </Container>
+);
 
 export default DownloadMaterial;
