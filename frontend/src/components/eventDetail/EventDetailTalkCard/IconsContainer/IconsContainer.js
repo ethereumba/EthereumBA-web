f2016d2f91f9dd7ebec1b1f3ae8f695cb726d732
niconiahi <nicolas.accetta@gmail.com>
2019-09-27 14:49:47 -0300

    ETHBA-55 WIP finish talk component with multiple displaying states

--- frontend/src/components/eventDetail/EventDetailTalkCard/IconsContainer/IconsContainer.js
+++ frontend/src/components/eventDetail/EventDetailTalkCard/IconsContainer/IconsContainer.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from "react";
 
 // styles
-import { Container } from './styles'
+import { Container } from "./styles";
 
 // components
-import IconTag from './IconTag'
+import IconTag from "./IconTag";
 
 // assets
-import BubbleIcon from '../../../../assets/eventDetail/chat-bubble-purple.svg'
-import MediumLevelIcon from '../../../../assets/eventDetail/medium-level-purple.svg'
+import BubbleIcon from "../../../../assets/eventDetail/chat-bubble-purple.svg";
+import MediumLevelIcon from "../../../../assets/eventDetail/medium-level-purple.svg";
 
 const IconsContainer = ({ talk }) => {
-    const resolveLevelIcon = () => {
-        switch (talk.level) {
-            case 'INITIAL':
-                return MediumLevelIcon;
-            case 'MEDIUM':
-                return MediumLevelIcon;
-            case 'ADVANCED':
-                return MediumLevelIcon;
-            default:
-                return MediumLevelIcon;
-        }
+  const resolveLevelIcon = () => {
+    switch (talk.level) {
+      case "INITIAL":
+        return MediumLevelIcon;
+      case "MEDIUM":
+        return MediumLevelIcon;
+      case "ADVANCED":
+        return MediumLevelIcon;
+      default:
+        return MediumLevelIcon;
     }
+  };
 
-    return (
-        <Container>
-            {talk.language && <IconTag icon={BubbleIcon} text={talk.language} />}
-            {talk.level && <IconTag icon={resolveLevelIcon()} text={talk.level} />}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      {talk.language && <IconTag icon={BubbleIcon} text={talk.language} />}
+      {talk.level && <IconTag icon={resolveLevelIcon()} text={talk.level} />}
+    </Container>
+  );
+};
 
-export default IconsContainer 
+export default IconsContainer;
