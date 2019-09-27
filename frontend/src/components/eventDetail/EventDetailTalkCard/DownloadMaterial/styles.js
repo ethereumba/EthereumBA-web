ba729899d72af5f7559a4771b79ed3ce5aee8054
niconiahi <nicolas.accetta@gmail.com>
2019-09-27 14:49:47 -0300

    ETHBA-55 WIP finish talk component with multiple displaying states

---
+++ frontend/src/components/eventDetail/EventDetailTalkCard/DownloadMaterial/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+// lib
+import { COLORS } from '../../../../lib/styles'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 12px;
+`
+
+export const IconWrapper = styled.div`
+  height: 19px;
+  width: 19px;
+`
+
+export const Icon = styled.img`
+  height: 100%;
+  width: 100%;
+
+  :hover {
+      cursor: pointer;
+  }
+`
+
+export const Text = styled.span`
+  color: ${COLORS.orangeText};
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 24px;
+`
