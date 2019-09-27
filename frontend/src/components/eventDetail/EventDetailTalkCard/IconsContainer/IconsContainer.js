3f63af724d2219af5d2a43032ac1ce21f4024fc6
niconiahi <nicolas.accetta@gmail.com>
2019-09-27 15:19:58 -0300

    ETHBA-55 DONE add logic to resolve correct icon based on talk level

--- frontend/src/components/eventDetail/EventDetailTalkCard/IconsContainer/IconsContainer.js
+++ frontend/src/components/eventDetail/EventDetailTalkCard/IconsContainer/IconsContainer.js
@@ -8,13 +8,26 @@ import IconTag from './IconTag'
 
 // assets
 import BubbleIcon from '../../../../assets/eventDetail/chat-bubble-purple.svg'
-import LevelIcon from '../../../../assets/eventDetail/level-purple.svg'
+import MediumLevelIcon from '../../../../assets/eventDetail/medium-level-purple.svg'
 
 const IconsContainer = ({ talk }) => {
+    const resolveLevelIcon = () => {
+        switch (talk.level) {
+            case 'INITIAL':
+                return MediumLevelIcon;
+            case 'MEDIUM':
+                return MediumLevelIcon;
+            case 'ADVANCED':
+                return MediumLevelIcon;
+            default:
+                return MediumLevelIcon;
+        }
+    }
+
     return (
         <Container>
             {talk.language && <IconTag icon={BubbleIcon} text={talk.language} />}
-            {talk.level && <IconTag icon={LevelIcon} text={talk.level} />}
+            {talk.level && <IconTag icon={resolveLevelIcon()} text={talk.level} />}
         </Container>
     )
 }
