b232adc7045a07b381e37fa5b32905c0cfbc532c
Nahuel Fernandez <naha90@gmail.com>
2019-10-30 11:56:36 -0300

    ETHBA-80 DONE

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.template import RequestContext
 
 
-def handler403(request, reason=""):
+def handler403(request, *args, **kwargs):
     response = render(request, '403.html')
     response.status_code = 403
     return response
 
 
-def handler404(request):
+def handler404(request, *args, **kwargs):
     response = render(request, '404.html')
     response.status_code = 404
     return response
 
 
-def handler500(request):
+def handler500(request, *args, **kwargs):
     response = render(request, '500.html')
     response.status_code = 500
     return response
